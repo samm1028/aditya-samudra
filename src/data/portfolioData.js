1492dc4b2407066382d8439f5a2843252d87fa39
Aditya Samudra <adityaadityaaditya000@gmail.com>
2026-04-19 08:42:24 +0700

    Update portfolioData.js

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -43,7 +43,7 @@ export const portfolioData = {
       title: "Company Profil Website - PT. Treemas Solusi Utama",
       description: "Saya telah membuat website company profile untuk PT. Treemas Solusi Utama menggunakan framework React.js dan Tailwind CSS. Website ini menampilkan informasi mengenai perusahaan, layanan yang ditawarkan, portofolio proyek, dan kontak.",
       tech: ["React.js", "HTML", "Bootstrap 5.3.0", "Tailwind"],
-      image: "/images/treemas.png",
+      image: "images/treemas.png",
       link: "https://samm1028.github.io/treemas-solusi-utama/",  // ← Tambahkan link
       github: "https://github.com/samm1028/treemas-solusi-utama.git"  // ← Opsional: link GitHub
     },
@@ -51,7 +51,7 @@ export const portfolioData = {
       title: "Website penyedia jasa joki & website pribadi",
       description: "Saya membuat website pribadi yang bertujuan untuk menampilkan informasi media sosial serta memudahkan orang dalam menggunakan layanan pembuatan (joki) website yang saya sediakan.",
       tech: ["HTML", "CSS", "JavaScript"],
-      image: "/images/pribadi.png",
+      image: "images/pribadi.png",
       link: "https://portfoilio-aditya-samudra.netlify.app/",  // ← Tambahkan link
       github: "https://github.com/samm1028/joki-website.git"
     },
@@ -59,7 +59,7 @@ export const portfolioData = {
       title: "Web perusahaan (Sky Tech)",
       description: "Ini adalah website company profile yang dibuat berdasarkan permintaan klien. Dalam proses pengembangannya, saya menggunakan HTML, CSS, dan JavaScript untuk membangun struktur, tampilan, serta interaksi pada website tersebut. Website ini dirancang agar informatif, responsif, dan mudah diakses, sehingga dapat merepresentasikan identitas perusahaan klien secara profesional.",
       tech: ["HTML", "CSS", "JavaScript"],
-      image: "/images/skytech.png",
+      image: "images/skytech.png",
       link: "https://samm1028.github.io/project-customer/",  // ← Tambahkan link
       github: "https://github.com/samm1028/project-customer.git"
     },
@@ -67,7 +67,7 @@ export const portfolioData = {
       title: "Certificate dicoding (Web Development)",
       description: "Saya telah mengikuti program bootcamp yang diselenggarakan oleh Dicoding. Pada program tersebut, saya memperoleh sertifikat \"Belajar Dasar Web\" setelah berhasil menyelesaikan seluruh quest yang diberikan.",
       tech: ["Certificate", "Artificial Intelligence", "Dicoding"],
-      image: "/images/sertiweb.png",
+      image: "images/sertiweb.png",
       link: "https://drive.google.com/file/d/1wonfGYqwZo0zJ1ddVATct6M_BI904tZT/view?usp=drive_link",  // ← Link sertifikat
       github: null  // ← Tidak ada GitHub untuk sertifikat
     },
@@ -75,7 +75,7 @@ export const portfolioData = {
       title: "Certificate dicoding (AI)",
       description: "Saya telah mengikuti program bootcamp yang diselenggarakan oleh Dicoding. Pada program tersebut, saya memperoleh sertifikat \"Belajar Dasar AI\" setelah berhasil menyelesaikan seluruh quest yang diberikan.",
       tech: ["Certificate", "Web Development", "Dicoding"],
-      image: "/images/ai.png",
+      image: "images/ai.png",
       link: "https://drive.google.com/file/d/1FKbyXXJIgNoKB-OhtsFeQwaVoM9EvjqA/view?usp=drive_link",  // ← Link sertifikat
       github: null
     },
@@ -83,7 +83,7 @@ export const portfolioData = {
       title: "Certificate lomba (Fotografi)",
       description: "Convert an input image to a pencil sketch using OpenCV and Matplotlib libraries.",
       tech: ["Certificate", "Fotografi"],
-      image: "/images/foto.png",
+      image: "images/foto.png",
       link: "https://drive.google.com/file/d/19QEyuTmr-kzj2R7iLrq2d9hTMbQDKmYb/view?usp=drive_link",  // ← Link sertifikat
       github: null
     }
